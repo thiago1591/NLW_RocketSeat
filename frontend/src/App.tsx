097b9017252b6d273e9c21c5c4b95f91e5866211
Thiago André <66754516+thiago1590@users.noreply.github.com>
2020-10-13 15:43:47 -0300

    Inicial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css'
+import { FiArrowRight} from 'react-icons/fi';
+
+import logoImg from './images/logo.svg';
+
+import Routes from './routes'
+
+function App() {
+  return (    
+    <Routes />
+  );
+}
+
+export default App;
