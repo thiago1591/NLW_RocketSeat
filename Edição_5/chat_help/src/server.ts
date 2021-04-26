32c06bb837b1ac67c672ba4c4b9a412ff2acc749
Thiago <thiagoandre2121@gmail.com>
2021-04-26 20:58:13 -0300

    chat_help typeorm configurado

--- Edição_5/chat_help/src/server.ts
+++ Edição_5/chat_help/src/server.ts
@@ -1,14 +1,13 @@
 import express from 'express'
+import './database'
+
+import {routes} from "./routes" 
 
 const app = express()
 
-app.get('/', (request,response) => {
-    return response.json({
-        'message':'Olá NLW'})
-})
+app.use(express.json())
 
-app.post('/', (request,response) => {
-    return response.json({message: "Usuario salvo com sucesso!"})
-})
+app.use(routes)
+
 
 app.listen(3333, () => console.log('server running on port 3333'))
