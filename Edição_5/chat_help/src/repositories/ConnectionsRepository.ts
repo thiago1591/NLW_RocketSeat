eed312d3b4212e3c843bf1752a23be22268a6477
Thiago <thiagoandre2121@gmail.com>
2021-06-07 13:58:57 -0300

    ✨  Added Connections(controller,service,routes)

---
+++ Edição_5/chat_help/src/repositories/ConnectionsRepository.ts
@@ -0,0 +1,10 @@
+import { Entity, Repository, EntityRepository } from 'typeorm';
+
+import {Connection} from '../entities/Connection'
+
+@EntityRepository(Connection)
+class ConnectionsRepository extends Repository<Connection> {
+    
+}
+
+export {ConnectionsRepository}
